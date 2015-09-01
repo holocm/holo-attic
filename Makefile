c03b9080085d6066c2d606c293941413fe6b7e34
Stefan Majewsky <majewsky@gmx.net>
2015-09-01 22:31:12 +0200

    setup build process for website
    `make website` will clone the website repo into a subdirectory and
    rebuild all the webpages from the doc/website-*.pod files. The result
    can then be committed and pushed in the website repo.

--- Makefile
+++ Makefile
@@ -21,3 +21,18 @@ install: build/holo src/holo-apply build/holo.8
 	install -D -m 0755 build/holo     "$(DESTDIR)/usr/bin/holo"
 	install -D -m 0755 src/holo-apply "$(DESTDIR)/usr/bin/holo-apply"
 	install -D -m 0755 build/holo.8   "$(DESTDIR)/usr/share/man/man8/holo.8"
+
+# the website is generated with pod2html (also from Perl) and a HTML template;
+# everything is mushed together using a small helper program
+build/holo-makewebsite: doc/makewebsite.go
+	go build -o $@ $<
+
+.PHONY: prepare-website-repo
+prepare-website-repo:
+	@[ -d website/.git ] || git clone https://github.com/majewsky/majewsky.github.io website/
+
+website/%.html: doc/website-%.pod doc/template.html build/holo-makewebsite prepare-website-repo
+	build/holo-makewebsite $*
+
+.PHONY: website
+website: prepare-website-repo $(patsubst doc/website-%.pod,website/%.html,$(wildcard doc/website-*.pod))
---
+++ doc/makewebsite.go
@@ -0,0 +1,141 @@
+/*******************************************************************************
+*
+*   Copyright 2015 Stefan Majewsky <majewsky@gmx.net>
+*
+*   This program is free software; you can redistribute it and/or modify it
+*   under the terms of the GNU General Public License as published by the Free
+*   Software Foundation; either version 2 of the License, or (at your option)
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful, but WITHOUT
+*   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+*   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+*   more details.
+*
+*   You should have received a copy of the GNU General Public License along
+*   with this program; if not, write to the Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*
+********************************************************************************/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+type InputData struct {
+	Title    string
+	Contents template.HTML
+	IsIndex  bool
+}
+
+func main() {
+	//first argument is the stem of the Makefile target (e.g. "index")
+	//from which all other paths are computed
+	stem := os.Args[1]
+	inputFile := "doc/website-" + stem + ".pod"
+	outputFile := "website/" + stem + ".html"
+
+	//run pod2html on the input file and read the result for post-processing
+	cmd := exec.Command("pod2html", "--noheader", "--noindex", "--infile="+inputFile)
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot execute pod2html: %s\n", err.Error())
+		return
+	}
+	contents := string(stdout.Bytes())
+
+	//use only <body>...</body> contents
+	contents = (regexp.MustCompile("<body>((?s:.+))</body>").FindStringSubmatch(contents))[1]
+	contents = strings.TrimSpace(contents)
+
+	//clean whitespace in <pre> elements
+	contents = regexp.MustCompile("<pre>((?s:.+?))</pre>").ReplaceAllStringFunc(contents, cleanWhitespaceInPre)
+
+	//divide into <section>s along <h1>
+	opener := "<section><div class=\"fixed-width clearfix\">"
+	closer := "</div></section>"
+	contents = opener + strings.Replace(contents, "<h1 ", closer+opener+"<h1 ", -1) + closer
+	//if output starts with a <h1>, we have a superfluous opener+closer at the start
+	contents = strings.TrimPrefix(contents, opener+closer)
+
+	//prepare input data for template processing
+	inputData := InputData{
+		Title:    "Holo - Minimalistic Configuration Management", //TODO: title per page
+		Contents: template.HTML(contents),
+		IsIndex:  stem == "index",
+	}
+
+	//read output template
+	templateString, err := ioutil.ReadFile("doc/template.html")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot read doc/template.html: %s\n", err.Error())
+		return
+	}
+	template, err := template.New("html").Parse(string(templateString))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot parse doc/template.html: %s\n", err.Error())
+		return
+	}
+
+	//write result to output file
+	file, err := os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot open %s: %s\n", outputFile, err.Error())
+		return
+	}
+	err = template.Execute(file, inputData)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot write %s: %s\n", outputFile, err.Error())
+		return
+	}
+}
+
+func cleanWhitespaceInPre(text string) string {
+	// trim leading whitespace in code snippets, e.g.
+	// input = "<pre><code>    foo\n      bar\n</code></pre>"
+	// output = "<pre><code>foo\n  bar\n</code></pre>"
+	text = strings.TrimSpace(text)
+	text = strings.TrimPrefix(text, "<pre>")
+	text = strings.TrimSuffix(text, "</pre>")
+	text = strings.TrimSpace(text)
+	text = strings.TrimPrefix(text, "<code>")
+	text = strings.TrimSuffix(text, "</code>")
+	lines := strings.Split(text, "\n")
+
+	//find shortest leading spaces run
+	minLeadingSpaces := 1000
+	for _, line := range lines {
+		for idx, runeVal := range line {
+			if runeVal != ' ' {
+				if minLeadingSpaces > idx {
+					minLeadingSpaces = idx
+				}
+				break
+			}
+		}
+	}
+
+	var trimmedLines []string
+	for _, line := range lines {
+		trimmedLines = append(trimmedLines, line[minLeadingSpaces:])
+	}
+	text = strings.Join(trimmedLines, "\n")
+
+	//while we're at it, we also fix that pod2html wrongly double escapes "&gt;" etc. in <pre>
+	text = strings.Replace(text, "&amp;gt;", "&gt;", -1)
+	text = strings.Replace(text, "&amp;lt;", "&lt;", -1)
+
+	return "<pre><code>" + text + "</code></pre>"
+}
