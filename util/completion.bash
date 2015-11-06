aa855894debf3f9131499cce9cf4467d08752ebd
Stefan Majewsky <majewsky@gmx.net>
2015-11-06 22:23:30 +0100

    fix a copy-paste error in the Bash completion

--- util/completion.bash
+++ util/completion.bash
@@ -5,13 +5,13 @@ _holo() {
 
     if [ "$COMP_CWORD" = 1 ]; then
         # autocomplete first argument (either a command verb or --help/--version)
-        COMPREPLY=( $(compgen -W "--help --version apply scan" -- "$CURRENT_WORD") )
+        COMPREPLY=( $(compgen -W "--help --version apply diff scan" -- "$CURRENT_WORD") )
         return 0
     elif [ "${COMP_WORDS[1]}" = "apply" ]; then
         # autocomplete for "holo apply" - argument is either an entity or -f/--force
         COMPREPLY=( $(compgen -W "$(holo scan --short) -f --force" -- "$CURRENT_WORD") )
         return 0
-    elif [ "${COMP_WORDS[1]}" = "apply" ]; then
+    elif [ "${COMP_WORDS[1]}" = "diff" ]; then
         # autocomplete for "holo diff" - argument is an entity
         COMPREPLY=( $(compgen -W "$(holo scan --short)" -- "$CURRENT_WORD") )
         return 0
